f6bacbe92db4b9663e579a44c796c088f6dd15c7
lhl <lhl@zpd.com>
2019-03-15 15:48:57 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s ${PWD}/.vimrc ~/.vimrc
 
 #install Vundle
 mkdir ~/.vim/bundle
-cp -af ${PWD}/Vundle.vim ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle
 
 #PluginInstall
 vim -c "PluginInstall"
